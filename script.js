7b33eea5af60ee54c4b7ab8853d34f39de1d2320
Muftau Saheed <horlanif@gmail.com>
2026-01-01 09:28:39 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function openMenu() {
 function closeMenu() {
     document.querySelector(".side-bar").classList.remove("active");
 }
+
+
+// ____________Emailjs_____________
+
+function sendMail(){
+    let parms ={
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject:document.getElementById("subject").value,
+        message:document.getElementById("message").value,
+    }
+
+    emailjs.send("service_532gpxa","template_n5nbxtb",parms).then(alert("Email Sent!"))
+
+    
+}
