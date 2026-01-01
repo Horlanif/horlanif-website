6338b18bfc2b091cb7354bbf21731439361fa996
Muftau Saheed <horlanif@gmail.com>
2025-12-31 23:31:55 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.querySelector(".side-bar").classList.add("active");
+}
+
+function closeMenu() {
+    document.querySelector(".side-bar").classList.remove("active");
+}
